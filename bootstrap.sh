928481f840c860efdd11cc6d7476a6ce3cffb26b
Martin Boren <martyboren@gmail.com>
2016-02-28 15:32:11 -0800

    Update Anaconda version to 2.5.0
     this script downloads the 64-bit, Python 2.7 version of the
     Anaconda 2.5.0 installer.

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,12 +45,12 @@ expect {
 # Anaconda comes with jupyter And a lot of useful Python data science stuff.
 # this is mostly borrowed from somebody else's vagrant scripts, but I don't
 # remember where it came from originally
-anaconda=Anaconda2-2.4.1-Linux-x86_64.sh
+anaconda=Anaconda2-2.5.0-Linux-x86_64.sh
 cd /vagrant
 echo -e "\n\nDownloading Anaconda installer. This may take more than a few minutes."
 if [ ! -s $anaconda ]
 then
-    wget -q -o /dev/null - https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.1-Linux-x86_64.sh
+    wget -q -o /dev/null - https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.5.0-Linux-x86_64.sh
 fi
 if [ -s $anaconda ]
 then
